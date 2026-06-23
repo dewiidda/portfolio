308ac3f91c3e6ec52de38330afa96bee66b37e5d
Dewi Idda <dewiidda.di@gmail.com>
2026-06-23 17:00:34 +0700

    create algorithms

---
+++ algo_visual/algorithms.py
@@ -0,0 +1,22 @@
+def bubble_sort(A):
+    n = len(A)
+    for i in range(n):
+        swapped = False
+        for j in range(0, n-i-1):
+            if A[j] > A[j+1]:
+                A[j], A[j+1] = A[j+1], A[j]
+                swapped = True
+            yield A
+        if not swapped:
+            break
+
+def linear_search(A, target=None):
+    if target is None:
+        target = random.choice(A)
+    for i in range(len(A)):
+        temp = list(A)
+        temp[i] = max(A) + 5
+        yield temp
+        if A[i] == target:
+            break
+        
